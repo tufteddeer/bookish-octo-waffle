6f3f500f42fe273a4bef8915ec0d32ee03572c9b
tufteddeer <tufteddeer@disroot.org>
2019-12-29 14:58:52 +0100

    convert whitespace to tabs

--- PlayerMovement.gd
+++ PlayerMovement.gd
@@ -6,11 +6,11 @@ var target = Vector2()
 var velocity = Vector2()
 
 func _process(delta):
-    if Input.is_action_pressed("click"):
-        target = get_global_mouse_position()
+	if Input.is_action_pressed("click"):
+		target = get_global_mouse_position()
 
 func _physics_process(delta):
-    velocity = (target - position).normalized() * speed
-    # rotation = velocity.angle()
-    if (target - position).length() > 5:
-        velocity = move_and_slide(velocity)
+	velocity = (target - position).normalized() * speed
+	# rotation = velocity.angle()
+	if (target - position).length() > 5:
+		velocity = move_and_slide(velocity)
