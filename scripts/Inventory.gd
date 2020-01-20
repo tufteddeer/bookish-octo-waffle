bb1fb8459bc4c228c110d2a38723b5efec1b237b
tufteddeer <tufteddeer@disroot.org>
2020-01-20 23:49:13 +0100

    use correct path

--- scripts/Inventory.gd
+++ scripts/Inventory.gd
@@ -5,7 +5,7 @@ var coins = 0
 var coinAmountLabel = null
 
 func _ready():
-	coinAmountLabel = get_node("/root/Node2D/UI/CanvasLayer/CoinValue")
+	coinAmountLabel = get_node("/root/Node2D/UI/UILayer/CoinValue")
 
 func addCoins(amount):
 	coins += amount
