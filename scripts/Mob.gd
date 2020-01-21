00ceaed0451dc635e1bbed7d5f14a98e753c8531
tufteddeer <tufteddeer@disroot.org>
2020-01-22 00:56:46 +0100

    add an epic diablo-like healthbar for big enemys

--- scripts/Mob.gd
+++ scripts/Mob.gd
@@ -10,6 +10,13 @@ onready var healthbar = $"Healthbar"
 export(int) var max_health = 10
 var health
 
+#are we currently using the big health display?
+var boss_healthbar_active = false
+onready var boss_display = $"/root/Node2D/UI/UILayer/boss_display"
+onready var boss_healthbar = boss_display.get_node("healthbar")
+onready var boss_name_label = boss_display.get_node("name")
+onready var boss_health_label = boss_display.get_node("boss_health")
+
 func _ready():
 	health = max_health
 	healthbar.max_value = max_health
@@ -20,6 +27,9 @@ func hit(damage):
 	
 	healthbar.value = health
 
+	if boss_healthbar_active && health > 0:
+		update_boss_healthbar()
+		
 	_hit()
 	if health <= 0:
 		die()
@@ -35,11 +45,28 @@ func die():
 	$AnimationPlayer.play("die")
 	$CollisionShape2D.disabled = true
 	healthbar.visible = false
+	disable_boss_healthbar()
 	_die()
-	
+
+func enable_boss_healthbar():
+	boss_healthbar_active = true
+	boss_display.visible = true
+	update_boss_healthbar()
+
+func update_boss_healthbar():
+	boss_healthbar.value = health
+	boss_healthbar.max_value = max_health
+	boss_health_label.text = String(health) + "/" + String(max_health)
+	boss_name_label.text = mob_type
+
+func disable_boss_healthbar():
+	boss_healthbar_active = false
+	boss_display.visible = false
+
 # This functions can be overwritten by inheriting objects
 func _hit():
 	pass
 
 func _die():
+	disable_boss_healthbar()
 	pass
--- scripts/minotaur.gd
+++ scripts/minotaur.gd
@@ -6,3 +6,12 @@ func _hit():
 
 func _die():
 	print("minotaur died :(")
+
+#enable the epic boss healtbar when the player is near
+func _on_nearby_body_entered(body):
+	if body.name == "Player" and health > 0:
+		enable_boss_healthbar()
+		
+func _on_nearby_body_exited(body):
+	if body.name == "Player":
+		disable_boss_healthbar()
