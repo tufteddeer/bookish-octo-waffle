7260bf9722cacb5344727fa107a0f36309c02662
tufteddeer <tufteddeer@disroot.org>
2020-01-21 13:52:32 +0100

    remove unneeded scripts

--- scripts/Lootable.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-const Coin = preload("res://objects/Coin.tscn")
-
-func onDie():
-	var node = Coin.instance()
-	node.set_global_position(get_global_position())
-	get_tree().get_current_scene().add_child(node)
